4603e53e0b65fedff1a56727e97ffb58b2896efd
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-05-17 23:57:42 +0300

    Create 3.2.cpp

---
+++ SHEETS/SHEET 2/3.2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+/*
+ 3. What is the output of the following code?
+ int a = 10;
+ int b = 20;
+ cout << (a > b ? a : b);
+ a) 10
+ b) 20
+ c) 30
+ d) Error
+*/
+int main(){
+ int a = 10;
+ int b = 20;
+ cout << (a > b ? a : b);
+//??means if (a>b)return a ,else return b  
+return 0;
+};
