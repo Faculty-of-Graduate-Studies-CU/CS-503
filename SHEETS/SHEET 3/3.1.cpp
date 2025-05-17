90fdad4f3831d77b97a2b9885f721892d5ac9e8b
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-05-18 00:52:07 +0300

    Update 3.1.cpp

--- SHEETS/SHEET 3/3.1.cpp
+++ SHEETS/SHEET 3/3.1.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
+/*
+1. Write a program (using switch case) that ask the user to enter the item number and prints the price of this item according to the following :
+No 1 price 100,
+NO 2 price 200,
+No 3 price 300,
+No 4 price 400
+*/
+int main(){
+int no;
+cout<<"Please Enter Item Number"<<endl;
+cin>>no;
+switch(no){
+case 1:
+cout<<"The Item Price iS 100"<<endl;
+case 2:
+cout<<"The Item Price iS 200"<<endl;
+break;
+
+case 3:
+cout<<"The Item Price iS 300"<<endl;
+break;
+
+default:
+cout<<"The Item Price iS 400"<<endl;
+break;
+};
+};
+//you will have not to determine the value or contain of it in no
+//you will have to evaluate number of cases
+//if you entered any number above 3 it will give you 400
 
