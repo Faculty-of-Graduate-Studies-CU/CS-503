95cda782072303a62fd8d5354ac8871226c1030e
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-05-17 20:29:27 +0300

    Update 2.cpp

--- SHEETS/SHEET 1/2.cpp
+++ SHEETS/SHEET 1/2.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using namespace std;
+//2. Write a program that calculate Kelvin and Fahrenheit from a Celsius degree entered by the user.
+/*
+k = c + 273
+f = c * 5/9 + 3
+*/
+int main(){
+float  c;
+float k;
+float f;
+cout <<"Please enter the degree in Celesius "<<endl;
+cin>>c;
+k = c +273;
+f = (c *9/5)+32;
+cout <<"The degree in kelvin is : "<<k<<endl;
+cout<<"The Degree in fahernheit is : "<<f <<endl;
+cout<<"\n";
+cout<<"\n";
+cout<<"\n";
+cout<<"\n";
+
+cout<<"Thanks for using our programme"<<endl;
+return 0;
+};
 
