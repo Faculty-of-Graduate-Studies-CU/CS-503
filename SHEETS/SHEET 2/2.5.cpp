833f4a67c01d544b1f49bb0bc0ac0f0c9727b0d8
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-05-18 00:35:34 +0300

    Update 2.5.cpp

--- SHEETS/SHEET 2/2.5.cpp
+++ SHEETS/SHEET 2/2.5.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+/*
+
+*/
+int main(){
+ int x = 7;
+ if (x > 5)
+//if(x < 10)
+ cout << "x is between 5 and 10";
+ else if(x <10 )
+ cout << "x is greater than or equal to 10";
+ else
+ cout << "x is less than or equal to 5";
+return 0;
+//first my answer was Error but if we look closw
+//we will see that we used else instead of else if and that make us evaluate every condition
+//we can use else if instead of two if Conditions
+//The Answer is x Between 7 and 10 
+};
 
