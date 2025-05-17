5a7071c827ae923b22e47d63df0a429be77a853d
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-05-17 20:15:52 +0300

    Update 1.cpp

--- SHEETS/SHEET 1/1.cpp
+++ SHEETS/SHEET 1/1.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+//1. Write a c ++ program to find the volume and surface area of sphere.
+/*
+Hint: PI=3.14
+volume=4.0/3 *pi*r*r*r;
+area=4*pi*r*r;
+*/
+int main(){
+float PI = 3.14;
+float r;
+float Volume = 4/3 * PI * r * r * r ;
+float area = 4 * PI * r * r ;
+cout<<"What is the radius of the sphere ?"<<endl;
+cin>> r;
+cout<<"The Volume of the sphere is : "<<Volume<<endl;
+cout<<"The Area of the sphere is : "<<area<<endl;
+cout<<"Thanks for using our programme"<<endl;
+return 0;
+};
