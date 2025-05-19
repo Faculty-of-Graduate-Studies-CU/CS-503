409c96251bf21cb3fcb351d94190b90b16f2e2b1
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-05-19 10:01:32 +0300

    Update factorial rec func.cpp

--- SHEETS/SHEET 4/factorial rec func.cpp
+++ SHEETS/SHEET 4/factorial rec func.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+int factorial(int n);
+
+
+int main(){
+int num;
+cout<<"Enter Positive Number"<<endl;
+cin>>num;
+if(num < 0){
+    cout<<"factorial is not defined for negative number"<<endl;
+}else{
+    cout<<factorial(num)<<endl;
+}
+return 0;
+};
+int factorial(int n){
+if(n > 1)
+    return n * factorial(n - 1);
+  else
+    return 1;
+};
 
