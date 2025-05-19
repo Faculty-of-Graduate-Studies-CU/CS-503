51f29328beada53b1d7802e1d718317a9d057d82
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-05-19 10:44:45 +0300

    Update 4.2.cpp

--- SHEETS/SHEET 4/4.2.cpp
+++ SHEETS/SHEET 4/4.2.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+/*
+2. Write a program in C++ to find the Greatest Common Divisor (GCD) of a number.
+Sample Output:
+Input a number: 15
+The Greatest Common Divisor is: 5
+*/
+
+int main(){
+int num, gcd;
+cout<<"Please Enter number of 2 or three digit\n";
+cin>>num;
+for(int i=2; i<=num/2; i++)
+    if (num % i == 0){
+        gcd = i;
+    }
+cout<<"The Greatest Common Divisor is : "<<gcd<<endl;
+
+return 0;
+};
 
